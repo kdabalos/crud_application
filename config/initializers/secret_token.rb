72474c7eae170db0663c8b14e2c48d60abeba68e
Kathrina Dabalos <kathlua05@gmail.com>
2013-05-21 21:55:11 +0300

    Initial commit of Kat's application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrudApplication::Application.config.secret_token = 'e1d05e415edc9ecd6b3a8a54ea6baed12c4592b24f86eb72dcfccb80ce5be48106f52351a092802fa5dec502dae56805dc95d05700c273f741887d34afe302db'
